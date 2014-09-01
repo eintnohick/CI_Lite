68ccc1a1747677981b78d6b0e55859e63d08607c
eintnohick <eintnohick@gmail.com>
2014-09-01 17:09:06 -0400

    more tweaking
    added some bootsrap

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ CI_Lite
 
 A slimmed down version of Codeigniter 3.0-dev with some formatting changes.
 
+## Major/Noticable changes
 
+- Removed user_guide_src/ and tests/
+- Simple integration with Bootstrap
+- All code comments have been removed
--- app/views/welcome_message.php
+++ app/views/welcome_message.php
@@ -1,88 +1,26 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?><!DOCTYPE html>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?><!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style>
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-	::-webkit-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<title>Welcome to CodeIgniter</title>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<style>
+body{
+	padding-top:40px;
+}
+</style>
 </head>
 <body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>app/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>app/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Welcome to CI_Lite!</h1>
+		<p>CI_Lite is a slimmed down version of <a href="https://github.com/EllisLab/CodeIgniter">Codeigniter 3.0-dev</a>.</p>
+		<p>If you would like to edit this page you'll find it located at: <kbd>app/views/welcome_message.php</kbd></p>
+		<p>The corresponding controller for this page is found at: <kbd>app/controllers/Welcome.php</kbd></p>
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="text-center">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' :'' ?></p>
 </div>
-
 </body>
 </html>
